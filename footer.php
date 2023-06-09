fb609e3b17baa231e65b55b0823f839e06f5f8ab
Nuffl3s <113404329+Nuffl3s@users.noreply.github.com>
2023-06-09 19:14:01 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,49 @@
+<section class="section-7">
+            <footer class="footer-distributed">
+
+                <div class="footer-left">
+                    <h3> <img src="images/Logo.png"> Cook<span>Folio</span></h3>
+
+                    <p class="footer-links">
+                        <a href="#">Home</a>
+                        |
+                        <a href="#">Recipe</a>
+                        |
+                        <a href="#">Contact</a>
+                        |
+                        <a href="#">About</a>
+                    </p>
+                </div>
+
+                <div class="footer-center">
+                    <div>
+                        <i class="fa fa-map-marker"></i>
+                        <p><span>Cagayan de Oro City</span>Misamis Oriental</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-phone"></i>
+                        <p>+63 9754247640</p>
+                    </div>
+                    <div>
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="#">CookF.@gmail.com</a></p>
+                    </div>
+                </div>
+
+                <div class="footer-right">
+                    <p class="about-cf">
+                        <span>About CookFolio</span>
+                        <strong>CookFolio</strong> is a website dedicated to food lovers, providing recipes, cooking tips, and articles to inspire and empower readers to cook and explore new flavors. Join us on this culinary journey and discover the joys of cooking together.
+                    </p>
+                    <div class="social">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                    </div>
+                </div>
+            </footer>
+            <div class="footer-2">
+                <p>COPYRIGHT© 2023 COOKFOLIO | ALL RIGHT RESERVED</p>
+            </div>
+         </section>
---
+++ head.php
@@ -0,0 +1,21 @@
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Title -->
+        <title>CookFolio</title>
+        <link rel="icon" href="images/logo.png" type="image">
+
+        <!-- style -->
+        <link rel="stylesheet" href="/CookFolio/css/GlobalStyle.css">
+        <link rel="stylesheet" href="/CookFolio/css/contact.css">
+
+        <!-- fontawesome | boxicons-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+
+        <!-- Google fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Lemon&family=Open+Sans:ital,wght@0,300;0,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
